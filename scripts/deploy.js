88a6ee5543d25721323ba8c10717ce89a71fb115
tingyuan <hbtwhucsis@gmail.com>
2021-12-11 02:33:25 +0800

    chore

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,6 +1,43 @@
 // 用于github actions自动部署到github pages
-const core = require('@actions/core')
+// const core = require('@actions/core')
 const {exec} = require('@actions/exec')
+const {rmRF, mkdirP, cp} = require('@actions/io')
+const path = require('path')
 
 console.log(__filename, process.cwd())
-// exec('git', [''])
+
+async function main () {
+  const docs = path.join(__dirname, 'docs')
+  const dist = path.join(__dirname, 'dist')
+  await exec('git', ['checkout', 'gh'])
+  console.log('git checkout to gh')
+  await rmRF(docs)
+  console.log('remove old docs dir')
+  await mkdirP(docs)
+  console.log('make new docs dir')
+  await cp(dist, docs, {
+    recursive: true, force: true
+  })
+  console.log('copy dist to docs')
+  await exec('git', ['add', '.'])
+  console.log('git add all files')
+  const buildTime = new Intl.DateTimeFormat('zh', {
+    year: 'numeric',  
+    month: '2-digit',
+    day: '2-digit',
+    hour: '2-digit',
+    minute: '2-digit',
+    hour12: false
+  }).format(new Date())
+  await exec('git', ['commit', '-m', `Build at ${buildTime}`])
+  console.log('git commit')
+  await exec('git', ['push', 'origin', 'gh'])
+  console.log('git push to origin gh')
+}
+
+main().catch(err => {
+  console.log('部署失败')
+  console.error(err)
+})
+
+
