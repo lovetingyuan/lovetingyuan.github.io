435103233520ca375e91608bb18ee34f8181188c
tingyuan <1932294867@qq.com>
2021-12-12 18:40:52 +0800

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -7,9 +7,6 @@ import buildInfo from './scripts/buildInfo'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  build: {
-    outDir: 'docs'
-  },
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
