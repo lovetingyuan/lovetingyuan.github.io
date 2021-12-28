9f5b496ce8c2c128a4046f244a1981984d6ff2c6
tingyuan <1932294867@qq.com>
2021-12-28 20:20:15 +0000

    Build at 2021/12/29 04:20

--- docs/assets/promise-polyfill.c2ba39f9.js
+++ docs/assets/promise-polyfill.c2ba39f9.js
@@ -1,9 +1,5 @@
 var e=`<h2 id="promise\u7684polyfill\u5B9E\u73B0">Promise\u7684polyfill\u5B9E\u73B0</h2>
-<pre><code class="language-javascript">// \u7528\u6765resolve\u4E00\u4E2A\u503C\uFF0C\u8FD9\u4E2A\u503C\u53EF\u80FD\u662Fpomise,thenable\u6216\u8005\u5176\u4ED6
-function resolveValue (promise, value, resolve, reject) {
-  if (promise === value) { // \u4E0D\u80FDresolve\u81EA\u8EAB
-    return reject(new TypeError(&#39;Can not resolve or return the current promise.&#39;))
-  }
+<pre><code class="language-javascript">function resolveValue (value, resolve, reject) {
   if (value === null || (typeof value !== &#39;object&#39; &amp;&amp; typeof value !== &#39;function&#39;)) {
     return resolve(value)
   }
@@ -13,44 +9,60 @@ function resolveValue (promise, value, resolve, reject) {
   } catch (err) {
     return reject(err)
   }
-  if (typeof then !== &#39;function&#39;) return resolve(value)
+  if (typeof then !== &#39;function&#39;) {
+    return resolve(value)
+  }
   let called = false // \u6240\u6709\u7684\u56DE\u8C03\u53EA\u80FD\u8C03\u7528\u4E00\u6B21
   try { // \u5904\u7406thenable\uFF0C\u5F53\u7136promise\u672C\u8EAB\u4E5F\u662Fthenable
     then.call(value, val =&gt; {
-      if (called) return
-      called = true
-      resolveValue(promise, val, resolve, reject) // \u9700\u8981\u9012\u5F52resolve\uFF0C\u56E0\u4E3A\u53EF\u80FD\u591A\u6B21\u8FD4\u56DEthenable
-    }, err =&gt; {
-      if (called) return
-      called = true
-      reject(err) // reject\u5C31\u76F4\u63A5\u8C03\u7528\u5373\u53EF
+      if (!called) {
+        called = true
+        resolveValue(val, resolve, reject) // \u9700\u8981\u9012\u5F52resolve\uFF0C\u56E0\u4E3A\u53EF\u80FD\u591A\u6B21\u8FD4\u56DEthenable
+      }
+    }, reason =&gt; {
+      if (!called) {
+        called = true
+        reject(reason) // reject\u5C31\u76F4\u63A5\u8C03\u7528\u5373\u53EF
+      }
     })
   } catch (err) {
-    if (!called) reject(err)
+    if (!called) {
+      reject(err)
+    }
   }
 }
 
 function Promise (callback) {
-  if (!(this instanceof Promise)) throw new TypeError(&#39;Promise cannot be invoked without &quot;new&quot;.&#39;)
-  if (typeof callback !== &#39;function&#39;) throw new TypeError(&#39;Promise callback is not a function.&#39;)
+  if (!(this instanceof Promise)) {
+    throw new TypeError(&#39;Promise cannot be invoked without &quot;new&quot;.&#39;)
+  }
+  if (typeof callback !== &#39;function&#39;) {
+    throw new TypeError(&#39;Promise callback is not a function.&#39;)
+  }
   this._status = &#39;pending&#39;
   this._value = undefined
   this._callbacks = { resolved: [], rejected: [] }
-  const settle = (status, value) =&gt; {
+  const fulfill = (status, value) =&gt; {
     this._value = value
     this._status = status
     this._callbacks[status].forEach(cb =&gt; cb(value))
   }
   let called = false
   const onResolve = value =&gt; {
-    if (called) return
-    called = true
-    resolveValue(this, value, val =&gt; settle(&#39;resolved&#39;, val), err =&gt; settle(&#39;rejected&#39;, err))
+    if (!called) {
+      called = true
+      if (this === value) {
+        fulfill(&#39;rejected&#39;, new TypeError(&#39;Can not resolve or return the current promise.&#39;))
+      } else {
+        resolveValue(value, fulfill.bind(null, &#39;resolved&#39;), fulfill.bind(null, &#39;rejected&#39;))
+      }
+    }
   }
-  const onReject = err =&gt; {
-    if (called) return
-    called = true
-    settle(&#39;rejected&#39;, err)
+  const onReject = reason =&gt; {
+    if (!called) {
+      called = true
+      fulfill(&#39;rejected&#39;, reason)
+    }
   }
   try {
     callback(onResolve, onReject)
@@ -60,32 +72,33 @@ function Promise (callback) {
 }
 
 Promise.prototype.then = function then (onResolve, onReject) {
-  const handleCallback = (promise, status, resolve, reject) =&gt; {
-    const callback = status === &#39;resolved&#39; ? onResolve : onReject
-    const settle = status === &#39;resolved&#39; ? resolve : reject
-    setTimeout(() =&gt; { // then\u7684\u56DE\u8C03\u9700\u8981\u5EF6\u8FDF\u6267\u884C\uFF0C\u5B9E\u9645\u5E94\u8BE5\u653E\u5230\u5FAE\u4EFB\u52A1\u961F\u5217\u4E2D
-      try {
-        if (typeof callback === &#39;function&#39;) {
-          resolveValue(promise, callback(this._value), resolve, reject)
-        } else {
-          settle(this._value)
+  const promise = new Promise((resolve, reject) =&gt; {
+    const handleCallback = (resolved) =&gt; {
+      setTimeout(() =&gt; { // then\u7684\u56DE\u8C03\u9700\u8981\u5EF6\u8FDF\u6267\u884C\uFF0C\u5B9E\u9645\u5E94\u8BE5\u653E\u5230\u5FAE\u4EFB\u52A1\u961F\u5217\u4E2D
+        const callback = resolved ? onResolve : onReject
+        if (typeof callback !== &#39;function&#39;) {
+          return (resolved ? resolve : reject)(this._value)
         }
-      } catch (err) {
-        reject(err)
-      }
-    })
-  }
-  let promise // then\u5FC5\u987B\u8FD4\u56DE\u4E00\u4E2A\u65B0\u7684promise
-  if (this._status === &#39;pending&#39;) { // \u5982\u679C\u662F\u5F02\u6B65\u6267\u884C\u9700\u8981\u5148\u628A\u56DE\u8C03\u5B58\u50A8\u5728\u961F\u5217\u4E2D
-    promise = new Promise((resolve, reject) =&gt; {
-      this._callbacks.resolved.push(() =&gt; handleCallback(promise, &#39;resolved&#39;, resolve, reject))
-      this._callbacks.rejected.push(() =&gt; handleCallback(promise, &#39;rejected&#39;, resolve, reject))
-    })
-  } else {
-    let resolve, reject
-    promise = new Promise((...args) =&gt; [resolve, reject] = args)
-    handleCallback(promise, this._status, resolve, reject)
-  }
+        let val
+        try {
+          val = callback(this._value)
+        } catch (err) {
+          return reject(err)
+        }
+        if (promise === val) {
+          reject(new TypeError(&#39;Can not resolve or return the current promise.&#39;))
+        } else {
+          resolveValue(val, resolve, reject)
+        }
+      })
+    }
+    if (this._status === &#39;pending&#39;) {
+      this._callbacks.resolved.push(() =&gt; handleCallback(true))
+      this._callbacks.rejected.push(() =&gt; handleCallback(false))
+    } else {
+      handleCallback(this._status === &#39;resolved&#39;)
+    }
+  })
   return promise
 }
 </code></pre>
