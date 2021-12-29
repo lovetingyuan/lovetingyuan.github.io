e1a12ac903252341105da6377e776ef659266cbc
tingyuan <hbtwhucsis@gmail.com>
2021-12-29 15:59:55 +0800

    chore

--- src/pages/home.vue
+++ src/pages/home.vue
@@ -1,23 +1,26 @@
 <template>
-<p>
-  <i style="margin-left: 1em">
-    ❄️ 随着海风吹，吹向来时庭院~
-  </i>
-</p>
-<br>
+  <p>
+    <i style="margin-left: 1em"> ❄️ 随着海风吹，吹向来时庭院~ </i>
+  </p>
+  <br />
   <ul>
     <li v-for="a of links" :key="a.url">
-      <a :href="a.url" target="_blank" rel="noopener noreferrer">{{a.title}}</a>
+      <a :href="a.url" target="_blank" rel="noopener noreferrer">{{
+        a.title
+      }}</a>
     </li>
   </ul>
 </template>
 
 <script setup lang="ts">
-const links = [{
-  title: '代码随想录',
-  url: 'https://programmercarl.com/'
-}, {
-  title: 'labuladong算法小抄',
-  url: 'https://labuladong.gitee.io/algo/'
-}]
+const links = [
+  {
+    title: "代码随想录",
+    url: "https://programmercarl.com/",
+  },
+  {
+    title: "labuladong算法小抄",
+    url: "https://labuladong.gitee.io/algo/",
+  },
+];
 </script>
