8433ff26f280f445980e2549bccb34991b4344ac
tingyuan <hbtwhucsis@gmail.com>
2021-12-29 15:58:58 +0800

    add some links

--- src/pages/home.vue
+++ src/pages/home.vue
@@ -4,4 +4,20 @@
     ❄️ 随着海风吹，吹向来时庭院~
   </i>
 </p>
+<br>
+  <ul>
+    <li v-for="a of links" :key="a.url">
+      <a :href="a.url" target="_blank" rel="noopener noreferrer">{{a.title}}</a>
+    </li>
+  </ul>
 </template>
+
+<script setup lang="ts">
+const links = [{
+  title: '代码随想录',
+  url: 'https://programmercarl.com/'
+}, {
+  title: 'labuladong算法小抄',
+  url: 'https://labuladong.gitee.io/algo/'
+}]
+</script>
