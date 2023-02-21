e018919e147029d16dbdb5727e4b2e724bbbc5d5
tingyuan <1932294867@qq.com>
2023-02-21 10:01:53 +0800

    chore

--- src/main.ts
+++ src/main.ts
@@ -3,12 +3,9 @@ import App from './App.vue'
 import createRouter from './router'
 import './style.css'
 import 'balloon-css'
+import './pwa'
 
 const app = createApp(App)
 const router = createRouter()
 app.use(router)
 app.mount('#app')
-
-if (!import.meta.env.SSR) {
-  import('./pwa')
-}
