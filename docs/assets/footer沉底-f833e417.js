6b6d684fdbb8df2c352a0ca4f9dfe87ddced6dc0
tingyuan <1932294867@qq.com>
2023-05-18 08:08:49 +0000

    Build at 2023/05/18 16:08

---
+++ docs/assets/footer沉底-f833e417.js
@@ -0,0 +1,43 @@
+import{o as n,c as a,p as l}from"./index-cb801410.js";const p={class:"markdown-body"},o=l(`<h3>方法一：将 footer 绝对定位到底部</h3><pre class="shiki dark-plus" style="background-color:#1E1E1E;" tabindex="0"><code class="language-html"><span class="line"><span style="color:#808080;">&lt;</span><span style="color:#569CD6;">div</span><span style="color:#D4D4D4;"> </span><span style="color:#9CDCFE;">class</span><span style="color:#D4D4D4;">=</span><span style="color:#CE9178;">&quot;wrapper&quot;</span><span style="color:#808080;">&gt;</span></span>
+<span class="line"><span style="color:#D4D4D4;">  </span><span style="color:#808080;">&lt;</span><span style="color:#569CD6;">div</span><span style="color:#D4D4D4;"> </span><span style="color:#9CDCFE;">class</span><span style="color:#D4D4D4;">=</span><span style="color:#CE9178;">&quot;content&quot;</span><span style="color:#808080;">&gt;</span><span style="color:#6A9955;">&lt;!-- 页面主体内容区域 --&gt;</span><span style="color:#808080;">&lt;/</span><span style="color:#569CD6;">div</span><span style="color:#808080;">&gt;</span></span>
+<span class="line"><span style="color:#D4D4D4;">  </span><span style="color:#808080;">&lt;</span><span style="color:#569CD6;">div</span><span style="color:#D4D4D4;"> </span><span style="color:#9CDCFE;">class</span><span style="color:#D4D4D4;">=</span><span style="color:#CE9178;">&quot;footer&quot;</span><span style="color:#808080;">&gt;</span><span style="color:#6A9955;">&lt;!-- 需要做到 Sticky Footer 效果的页脚 --&gt;</span><span style="color:#808080;">&lt;/</span><span style="color:#569CD6;">div</span><span style="color:#808080;">&gt;</span></span>
+<span class="line"><span style="color:#808080;">&lt;/</span><span style="color:#569CD6;">div</span><span style="color:#808080;">&gt;</span></span>
+<span class="line"></span>
+<span class="line"><span style="color:#808080;">&lt;</span><span style="color:#569CD6;">style</span><span style="color:#808080;">&gt;</span></span>
+<span class="line"><span style="color:#D4D4D4;">  </span><span style="color:#D7BA7D;">html</span><span style="color:#D4D4D4;">,</span></span>
+<span class="line"><span style="color:#D4D4D4;">  </span><span style="color:#D7BA7D;">body</span><span style="color:#D4D4D4;"> {</span></span>
+<span class="line"><span style="color:#D4D4D4;">    </span><span style="color:#9CDCFE;">height</span><span style="color:#D4D4D4;">: </span><span style="color:#B5CEA8;">100%</span><span style="color:#D4D4D4;">;</span></span>
+<span class="line"><span style="color:#D4D4D4;">  }</span></span>
+<span class="line"><span style="color:#D4D4D4;">  </span><span style="color:#D7BA7D;">.wrapper</span><span style="color:#D4D4D4;"> {</span></span>
+<span class="line"><span style="color:#D4D4D4;">    </span><span style="color:#9CDCFE;">position</span><span style="color:#D4D4D4;">: </span><span style="color:#CE9178;">relative</span><span style="color:#D4D4D4;">;</span></span>
+<span class="line"><span style="color:#D4D4D4;">    </span><span style="color:#9CDCFE;">min-height</span><span style="color:#D4D4D4;">: </span><span style="color:#B5CEA8;">100%</span><span style="color:#D4D4D4;">;</span></span>
+<span class="line"><span style="color:#D4D4D4;">    </span><span style="color:#9CDCFE;">padding-bottom</span><span style="color:#D4D4D4;">: </span><span style="color:#B5CEA8;">50px</span><span style="color:#D4D4D4;">;</span></span>
+<span class="line"><span style="color:#D4D4D4;">    </span><span style="color:#9CDCFE;">box-sizing</span><span style="color:#D4D4D4;">: </span><span style="color:#CE9178;">border-box</span><span style="color:#D4D4D4;">;</span></span>
+<span class="line"><span style="color:#D4D4D4;">  }</span></span>
+<span class="line"><span style="color:#D4D4D4;">  </span><span style="color:#D7BA7D;">.footer</span><span style="color:#D4D4D4;"> {</span></span>
+<span class="line"><span style="color:#D4D4D4;">    </span><span style="color:#9CDCFE;">position</span><span style="color:#D4D4D4;">: </span><span style="color:#CE9178;">absolute</span><span style="color:#D4D4D4;">;</span></span>
+<span class="line"><span style="color:#D4D4D4;">    </span><span style="color:#9CDCFE;">bottom</span><span style="color:#D4D4D4;">: </span><span style="color:#B5CEA8;">0</span><span style="color:#D4D4D4;">;</span></span>
+<span class="line"><span style="color:#D4D4D4;">    </span><span style="color:#9CDCFE;">height</span><span style="color:#D4D4D4;">: </span><span style="color:#B5CEA8;">50px</span><span style="color:#D4D4D4;">;</span></span>
+<span class="line"><span style="color:#D4D4D4;">  }</span></span>
+<span class="line"><span style="color:#808080;">&lt;/</span><span style="color:#569CD6;">style</span><span style="color:#808080;">&gt;</span></span>
+<span class="line"></span></code></pre><h3>方法二：为 footer 设置负 margin-top</h3><pre class="shiki dark-plus" style="background-color:#1E1E1E;" tabindex="0"><code class="language-html"><span class="line"><span style="color:#808080;">&lt;</span><span style="color:#569CD6;">div</span><span style="color:#D4D4D4;"> </span><span style="color:#9CDCFE;">class</span><span style="color:#D4D4D4;">=</span><span style="color:#CE9178;">&quot;content&quot;</span><span style="color:#808080;">&gt;&lt;/</span><span style="color:#569CD6;">div</span><span style="color:#808080;">&gt;</span></span>
+<span class="line"><span style="color:#808080;">&lt;</span><span style="color:#569CD6;">footer</span><span style="color:#D4D4D4;"> </span><span style="color:#9CDCFE;">class</span><span style="color:#D4D4D4;">=</span><span style="color:#CE9178;">&quot;footer&quot;</span><span style="color:#808080;">&gt;&lt;/</span><span style="color:#569CD6;">footer</span><span style="color:#808080;">&gt;</span></span>
+<span class="line"></span>
+<span class="line"><span style="color:#808080;">&lt;</span><span style="color:#569CD6;">style</span><span style="color:#808080;">&gt;</span></span>
+<span class="line"><span style="color:#D4D4D4;">  </span><span style="color:#D7BA7D;">html</span><span style="color:#D4D4D4;">,</span></span>
+<span class="line"><span style="color:#D4D4D4;">  </span><span style="color:#D7BA7D;">body</span><span style="color:#D4D4D4;"> {</span></span>
+<span class="line"><span style="color:#D4D4D4;">    </span><span style="color:#9CDCFE;">height</span><span style="color:#D4D4D4;">: </span><span style="color:#B5CEA8;">100%</span><span style="color:#D4D4D4;">;</span></span>
+<span class="line"><span style="color:#D4D4D4;">  }</span></span>
+<span class="line"><span style="color:#D4D4D4;">  </span><span style="color:#D7BA7D;">.content</span><span style="color:#D4D4D4;"> {</span></span>
+<span class="line"><span style="color:#D4D4D4;">    </span><span style="color:#9CDCFE;">min-height</span><span style="color:#D4D4D4;">: </span><span style="color:#B5CEA8;">100%</span><span style="color:#D4D4D4;">;</span></span>
+<span class="line"><span style="color:#D4D4D4;">    </span><span style="color:#9CDCFE;">padding-bottom</span><span style="color:#D4D4D4;">: </span><span style="color:#B5CEA8;">50px</span><span style="color:#D4D4D4;">;</span></span>
+<span class="line"><span style="color:#D4D4D4;">    </span><span style="color:#9CDCFE;">box-sizing</span><span style="color:#D4D4D4;">: </span><span style="color:#CE9178;">border-box</span><span style="color:#D4D4D4;">;</span></span>
+<span class="line"><span style="color:#D4D4D4;">  }</span></span>
+<span class="line"></span>
+<span class="line"><span style="color:#D4D4D4;">  </span><span style="color:#D7BA7D;">.footer</span><span style="color:#D4D4D4;"> {</span></span>
+<span class="line"><span style="color:#D4D4D4;">    </span><span style="color:#9CDCFE;">height</span><span style="color:#D4D4D4;">: </span><span style="color:#B5CEA8;">50px</span><span style="color:#D4D4D4;">;</span></span>
+<span class="line"><span style="color:#D4D4D4;">    </span><span style="color:#6A9955;">/* 下面的margin-top也可以转为对.content 设置 margin-bottom: -50px; */</span></span>
+<span class="line"><span style="color:#D4D4D4;">    </span><span style="color:#9CDCFE;">margin-top</span><span style="color:#D4D4D4;">: </span><span style="color:#B5CEA8;">-50px</span><span style="color:#D4D4D4;">;</span></span>
+<span class="line"><span style="color:#D4D4D4;">  }</span></span>
+<span class="line"><span style="color:#808080;">&lt;/</span><span style="color:#569CD6;">style</span><span style="color:#808080;">&gt;</span></span>
+<span class="line"></span></code></pre>`,4),t=[o],i={__name:"footer沉底",setup(D,{expose:s}){return s({frontmatter:{}}),(c,r)=>(n(),a("div",p,t))}};export{i as default};
