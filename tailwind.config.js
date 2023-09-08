d9bd7ce681dfd9174e06d679d5c9bda5d42f5f56
tingyuan <hbtwhucsis@gmail.com>
2023-09-08 20:32:38 +0800

    chore

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,8 @@
-const { addDynamicIconSelectors } = require('@iconify/tailwind')
-
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   darkMode: 'class',
   theme: {
     extend: {}
-  },
-  plugins: [addDynamicIconSelectors()]
+  }
 }
