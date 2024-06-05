0c3e0a75cb19b33108bae6eed5cfe80e00ffe813
tingyuan <1932294867@qq.com>
2024-06-05 15:31:31 +0800

    -

--- src/components/update-button.vue
+++ src/components/update-button.vue
@@ -2,5 +2,13 @@
   <div class="fixed bottom-[70px] right-4 cursor-pointer text-4xl hover:text-lime-600">
     <!-- <icon-material-symbols-refresh class="animate-spin" /> -->
     <iconify-icon icon="material-symbols:refresh" class="animate-spin"></iconify-icon>
+    <!-- <button @click="add">addd</button> -->
   </div>
 </template>
+
+<!-- <script setup>
+const add = () => {
+  let now = Date.now()
+  while(Date.now() - now < 2000)
+}
+</script> -->
